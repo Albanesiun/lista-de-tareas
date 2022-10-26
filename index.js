6065cf8f8e775db61bd2e44603db98efa4325f0d
nagykorte <nicolasmatray@hotmail.com>
2022-10-26 12:11:55 -0300

    add javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+var button
+window.addEventListener("load", function index () {
+    button = document.getElementById("button")
+})
+
+function main(e) {
+    e.preventDefault()
+    var task = document.getElementById("añadir")
+    console.log(añadir)
+    // alert("")
+}
+
+if (button) button.addEventListener("click", main)
